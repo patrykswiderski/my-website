e9378fed407fbf2100e4e5864772b244b0821396
Patryk <patryk.swiderski.85@gmail.com>
2024-04-11 15:46:36 +0200

    Footer update

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <div className='max-w-[1220px] sm:h-[150px] p-6 flex justify-between mx-auto'>
-        <span className='text-gray-300'>Patryk Świderski</span>
-        <p className='text-gray-600'>Copyright © 2024. All rights are reserved</p>
+        <span className='text-gray-300 text-xs sm:text-sm'>Patryk Świderski</span>
+        <p className='text-gray-600 text-xs sm:text-sm'>Copyright © 2024. All rights are reserved</p>
     </div>
   )
 }
