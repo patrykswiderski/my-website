b7d5f29c90bb2926981d74b3368c36f843fc1807
Patryk <patryk.swiderski.85@gmail.com>
2024-05-23 11:48:43 +0200

    Upgrades added

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,72 +1,77 @@
-"use client"
-import React from 'react'
-import {TypeAnimation} from "react-type-animation"
-import Image from 'next/image'
-import { IconBrandLinkedin,IconBrandGithub } from '@tabler/icons-react';
+"use client";
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import Image from "next/image";
+import { IconBrandLinkedin, IconBrandGithub } from "@tabler/icons-react";
 
 function Hero() {
   return (
-    <div>
-        <div className='grid grid-cols-1 max-w-[1200px] mx-auto bg-white'>
-            
-            <div className='flex flex-col justify-center items-center mx-auto my-auto w-auto h-auto'>
-                <img src='/best2.jpg' alt='hero image' className='lg:rounded-2xl'/>
-            </div>
+    <div className="mt-[80px]">
+      <div className="grid grid-cols-1 max-w-[1200px] mx-auto bg-white">
+        <div className="flex flex-col justify-center items-center mx-auto my-auto w-auto h-auto">
+          <img src="/best2.jpg" alt="hero image" className="lg:rounded-2xl" />
+        </div>
 
-            <div className='cols-span-2 px-5 my-auto'>
-                <h1 className='text-gray-800 text text-4xl sm:text-5xl lg:text-8xl font-extrabold'>
-                    <span className='primary-color'>
-                      I'm a
-                    </span> <br/>
-                    <TypeAnimation 
-                        sequence={[
-                          "Web dev",
-                          2000,
-                          "Front-End Dev",
-                          2000,
-                          "React Dev",
-                          2000,
-                        ]}
-                        wrapper="span"
-                        speed={40}
-                        repeat={Infinity}
-                    />
-                </h1>
+        <div className="cols-span-2 px-5 my-auto">
+          <h1 className="text-gray-800 text text-4xl sm:text-5xl lg:text-8xl font-extrabold">
+            <span className="primary-color">I'm a</span> <br />
+            <TypeAnimation
+              sequence={[
+                "Web dev",
+                2000,
+                "Front-End Dev",
+                2000,
+                "React Dev",
+                2000,
+              ]}
+              wrapper="span"
+              speed={40}
+              repeat={Infinity}
+            />
+          </h1>
 
-                <p className='text-gray-600 sm:text-lg my-6 lg:text-xl'>
-                    My name is Patryk Świderski. A passionate Front-end React Developer based in Warsaw, Poland.
-                </p>
+          <p className="text-gray-600 sm:text-lg my-6 lg:text-xl">
+            My name is Patryk Świderski. A passionate Front-end React Developer
+            based in Warsaw, Poland.
+          </p>
 
-                <div className='mx-auto grid grid-cols-2 place-items-center md:flex md:justify-start md:items-center gap-4 md:gap-4 my-8'>
-                    <a href='https://www.linkedin.com/in/patryk-świderski' className='transform transition-transform hover:scale-110'>
-                        <IconBrandLinkedin color='black' size={62}/>
-                    </a>
-                    
-                    <a href='https://github.com/patrykswiderski' className='transform transition-transform hover:scale-110 '>
-                        <IconBrandGithub color='black' size={54}/>
-                    </a>
+          <div className="mx-auto grid grid-cols-2 place-items-center md:flex md:justify-start md:items-center gap-4 md:gap-4 my-8">
+            <a
+              href="https://www.linkedin.com/in/patryk-świderski"
+              className="transform transition-transform hover:scale-110"
+            >
+              <IconBrandLinkedin color="black" size={62} />
+            </a>
 
-                    <a href='mailto:patryk.swiderski.85@gmail.com' className='col-span-2 w-full lg:w-auto font-bold lg:font-semibold text-gray-700 text-center px-6 py-3 rounded-xl lg:mr-4 border border-x-gray-400 hover:bg-gradient-to-br from-gray-500 to-gray-900 hover:text-white hover:border-none transform transition-transform hover:scale-105 shadow-md shadow-gray-500'>
-                        patryk.swiderski.85@gmail.com
-                    </a>
+            <a
+              href="https://github.com/patrykswiderski"
+              className="transform transition-transform hover:scale-110 "
+            >
+              <IconBrandGithub color="black" size={54} />
+            </a>
 
-                    <a href='/CV_Patryk_Swiderski_EN.pdf' className='col-span-2 w-full lg:w-auto text-center px-6 py-3 rounded-xl lg:mr-4 bg-gradient-to-br  from-pink-700 to-violet-900 text-white transform transition-transform hover:scale-105 hover:text-gray-800 hover:font-bold shadow-md shadow-gray-500'>
-                        Download CV
-                    </a>
-                    
-                    {/* <a href='#contact' className='col-span-2 w-full lg:w-auto text-center px-6 py-3 rounded-xl lg:mr-4 border border-x-gray-400 hover:bg-gradient-to-br from-orange-500 to-pink-500 text-white hoover:border-none transform transition-transform hover:scale-105'>
+            <a
+              href="mailto:patryk.swiderski.85@gmail.com"
+              className="col-span-2 w-full lg:w-auto font-bold lg:font-semibold text-gray-700 text-center px-6 py-3 rounded-xl lg:mr-4 border border-x-gray-400 hover:bg-gradient-to-br from-gray-500 to-gray-900 hover:text-white hover:border-none transform transition-transform hover:scale-105 shadow-md shadow-gray-500"
+            >
+              patryk.swiderski.85@gmail.com
+            </a>
+
+            <a
+              href="/CV_Patryk_Swiderski_EN.pdf"
+              className="col-span-2 w-full lg:w-auto text-center px-6 py-3 rounded-xl lg:mr-4 bg-gradient-to-br  from-pink-700 to-violet-900 text-white transform transition-transform hover:scale-105 hover:text-gray-800 hover:font-bold shadow-md shadow-gray-500"
+            >
+              Download CV
+            </a>
+
+            {/* <a href='#contact' className='col-span-2 w-full lg:w-auto text-center px-6 py-3 rounded-xl lg:mr-4 border border-x-gray-400 hover:bg-gradient-to-br from-orange-500 to-pink-500 text-white hoover:border-none transform transition-transform hover:scale-105'>
                         Contact
                     </a> */}
-                </div>
-            </div>
-
-
-    
-
-
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -4,6 +4,7 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
   const [nav, setNav] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
@@ -15,34 +16,83 @@ function Navbar() {
     }
   };
 
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
   useEffect(() => {
     document.addEventListener("keydown", handleCloseNav);
     document.addEventListener("click", handleCloseNav);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
       document.removeEventListener("keydown", handleCloseNav);
       document.removeEventListener("click", handleCloseNav);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [nav]);
+
+  const handleLinkClick = (event, sectionId) => {
+    event.preventDefault();
+    const section = document.querySelector(sectionId);
+    const navbarHeight = +55;
+
+    window.scrollTo({
+      top: section.offsetTop - navbarHeight,
+      behavior: "smooth",
+    });
+
+    setNav(false);
+  };
 
   const cssClassSm = "p-5 transform transition-transform hover:scale-110";
   const cssClassLg =
     "p-2 transform transition-transform hover:bg-primary-color hover:bg-gradient-to-br from-pink-800 to-violet-900 rounded-xl";
 
   return (
-    <div className="bg-white text-gray-800 font-semibold h-[80px] max-w-[1200px] mx-auto flex justify-between items-center">
-      <h1 className="text-3xl font-bold text-gray-800 ml-4">Patryk.dev</h1>
+    <div
+      className={`fixed top-0 left-0 right-0 font-semibold max-w-[1200px] mx-auto flex justify-between items-center shadow-md z-50 transition-all duration-300 ${
+        scrolled
+          ? "h-[50px] bg-gray-800 text-gray-100 rounded-b-md"
+          : "h-[80px] bg-white text-gray-800"
+      }`}
+    >
+      <h1
+        className={`ml-4 ${
+          scrolled
+            ? " text-gray-100 font-normal text-2xl"
+            : " text-gray-800 font-bold text-3xl"
+        }`}
+      >
+        Patryk.dev
+      </h1>
       <ul className="hidden md:flex">
-        <li className={cssClassSm}>
+        <li
+          className={cssClassSm}
+          onClick={(e) => handleLinkClick(e, "#skills")}
+        >
           <a href="#skills">Skills</a>
         </li>
-        <li className={cssClassSm}>
+        <li
+          className={cssClassSm}
+          onClick={(e) => handleLinkClick(e, "#projects")}
+        >
           <a href="#projects">Projects</a>
         </li>
-        <li className={cssClassSm}>
+        <li
+          className={cssClassSm}
+          onClick={(e) => handleLinkClick(e, "#about")}
+        >
           <a href="#about">About</a>
         </li>
-        <li className={cssClassSm}>
+        <li
+          className={cssClassSm}
+          onClick={(e) => handleLinkClick(e, "#contact")}
+        >
           <a href="#contact">Contact</a>
         </li>
       </ul>
@@ -61,16 +111,27 @@ function Navbar() {
         <h1 className="text-2xl sm:text-3xl text-gray-200 r m-4">Patryk.dev</h1>
         <ul className="p-2 text-xl sm:text-2xl text-gray-200">
           <li className={cssClassLg} onClick={handleNav}>
-            <a href="#skills">Skills</a>
+            <a href="#skills" onClick={(e) => handleLinkClick(e, "#skills")}>
+              Skills
+            </a>
           </li>
           <li className={cssClassLg} onClick={handleNav}>
-            <a href="#projects">Projects</a>
+            <a
+              href="#projects"
+              onClick={(e) => handleLinkClick(e, "#projects")}
+            >
+              Projects
+            </a>
           </li>
           <li className={cssClassLg} onClick={handleNav}>
-            <a href="#about">About</a>
+            <a href="#about" onClick={(e) => handleLinkClick(e, "#about")}>
+              About
+            </a>
           </li>
           <li className={cssClassLg} onClick={handleNav}>
-            <a href="#contact">Contact</a>
+            <a href="#contact" onClick={(e) => handleLinkClick(e, "#contact")}>
+              Contact
+            </a>
           </li>
         </ul>
       </div>
